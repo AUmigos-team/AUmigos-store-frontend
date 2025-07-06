ad6201ec9a9a4ec5c3f914b2af0892a10ddba431
Joana Elias de Oliveira <joana.oliverlias@outlook.com>
2025-07-06 18:01:46 -0300

    feat: add banner carousel component to home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdvantagesComponent } from './components/advantages/advantages.component';
+import {BannerCarouselComponent} from './components/banner-carousel/banner-carousel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, AdvantagesComponent],
+  imports: [CommonModule, AdvantagesComponent, BannerCarouselComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
